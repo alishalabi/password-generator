4e28cf6418f0f3e165b150fb0c4e7bd34e3c10d6
Ali Shalabi <ashalabi13@gmail.com>
2020-04-28 16:06:29 -0700

    Finished local storage config

--- src/App.js
+++ src/App.js
@@ -6,7 +6,37 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import reducers from './reducers';
 
-const store = createStore(reducers)
+const PASSWORD_STATE = "PASSWORD_STATE"
+
+// Load State From Local Storage
+export const loadState= () => {
+  try {
+    const serializedState = localStorage.getItem(PASSWORD_STATE)
+    if (serializedState === null) {
+      return undefined
+    }
+    return JSON.parse(serializedState)
+  } catch(err) {
+    return undefined
+  }
+}
+
+// Save State To Local Storage
+export const saveState = (state) => {
+  try {
+    const serializedState = JSON.stringify(state)
+    localStorage.setItem(PASSWORD_STATE, serializedState)
+  } catch(err) {
+    console.log("Error saving data:"+err)
+  }
+}
+
+// Initalize Store
+const persistedState = loadState();
+const store = createStore(reducers, persistedState)
+store.subscribe(() => {
+  saveState(store.getState())
+})
 
 
 class App extends Component {
